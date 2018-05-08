d00ef2fe5020848ff5a39453e57743e8a8820b1e
Daniel Legrand <daniel.legrand@s.elim.school.nz>
2018-05-09 10:13:14 +1200

    Gravity Flapping

--- Assets/scripts/TurtleMovemnt.cs
+++ Assets/scripts/TurtleMovemnt.cs
@@ -7,20 +7,30 @@ public class TurtleMovemnt : MonoBehaviour
 	public Sprite downSprite;
 
 	public Sprite upSprite;
+	public float jumpHeight;
 	private SpriteRenderer spriteRenderer;
+	private Rigidbody2D rB;
 	// Use this for initialization
 	void Start()
 	{
 		spriteRenderer = GetComponent<SpriteRenderer>();
 		spriteRenderer.sprite = upSprite;
+
+		rB = GetComponent<Rigidbody2D>();
 	}
 
 	// Update is called once per frame
-	void Update()
+	void Update()   
 	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			
+			rB.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+		}
 		if (Input.GetKey(KeyCode.Space))
 		{
 			spriteRenderer.sprite = downSprite;
+
 		}
 		else
 		{
